71e84a1c4bbf667e0102bf4898ac02be3d551979
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2021-09-01 04:08:48 -0300

    feat: rename functions

--- src/io.rs
+++ src/io.rs
@@ -11,14 +11,14 @@ pub fn check_mocks_dir() -> std::io::Result<()> {
     Ok(())
 }
 
-pub fn read_file(resource: &str) -> Result<String, std::io::Error> {
-    let filename = utils::filename(resource);
+pub fn read(resource: &str) -> Result<String, std::io::Error> {
+    let filename = utils::format_filename(resource);
     let file_content = fs::read_to_string(filename)?;
     Ok(file_content)
 }
 
-pub fn write_file(resource: &str, file_content: String) -> Result<(), std::io::Error> {
-    let filename = utils::filename(resource);
+pub fn write(resource: &str, file_content: String) -> Result<(), std::io::Error> {
+    let filename = utils::format_filename(resource);
 
     debug!("Write filename: {}", filename);
     Ok(fs::write(filename, file_content)?)
