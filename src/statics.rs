4af8fa2ffacef474ba306e8aab0fde8c75e3a943
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2021-10-13 21:00:25 -0300

    fix: remore statics autoimport

--- src/statics.rs
+++ src/statics.rs
@@ -4,13 +4,13 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use reqwest::Client;
 
-use crate::{config, latency, statics};
+use crate::{config, latency};
 
 lazy_static! {
     pub static ref API_REGEX: Regex = Regex::new(&ENVS.api_regex).unwrap();
 
     pub static ref CLIENT: reqwest::Client = Client::builder()
-        .user_agent(&statics::ENVS.h_user_agent)
+        .user_agent(&ENVS.h_user_agent)
         .timeout(time::Duration::from_millis(2000))
         .build()
         .unwrap();
@@ -19,9 +19,9 @@ lazy_static! {
     pub static ref ENVS: config::Envs = config::Envs::default();
 
     #[derive(Debug)]
-    pub static ref LATENCY_COLLECTION: latency::Collection = match statics::ENVS.latency_enable {
+    pub static ref LATENCY_COLLECTION: latency::Collection = match ENVS.latency_enable {
         true => {
-            serde_json::from_str(&fs::read_to_string(&statics::ENVS.latency_collection).unwrap())
+            serde_json::from_str(&fs::read_to_string(&ENVS.latency_collection).unwrap())
                 .unwrap()
         }
         false => HashMap::new(),
