41a5004f0db4e6784b3566f845f073c2aecb4e93
Rodolfo Mói de Oliveira <rodmoi.oliveira@gmail.com>
2021-10-14 01:21:44 -0300

    refactor: add statics for FUNES_ envs

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,14 @@ use serde::Serialize;
 
 use crate::error;
 
+pub static FUNES_ALLOW_EXTERNALS: &str = "FUNES_ALLOW_EXTERNALS";
+pub static FUNES_API_REGEX: &str = "FUNES_API_REGEX";
+pub static FUNES_APP: &str = "FUNES_APP";
+pub static FUNES_HOST: &str = "FUNES_HOST";
+pub static FUNES_LATENCY_COLLECTION: &str = "FUNES_LATENCY_COLLECTION";
+pub static FUNES_LOG: &str = "FUNES_LOG";
+pub static FUNES_MOCK_DIR: &str = "FUNES_MOCK_DIR";
+
 #[derive(Serialize, Clone, Debug)]
 pub struct Envs {
     pub allow_externals: bool,
@@ -20,22 +28,22 @@ pub struct Envs {
 impl Envs {
     pub fn default() -> Self {
         let latency_collection =
-            env::var("FUNES_LATENCY_COLLECTION").unwrap_or_else(|_| "".to_string());
+            env::var(FUNES_LATENCY_COLLECTION).unwrap_or_else(|_| "".to_string());
         let latency_enable = !latency_collection.is_empty();
 
         Envs {
-            allow_externals: env::var("FUNES_ALLOW_EXTERNALS")
+            allow_externals: env::var(FUNES_ALLOW_EXTERNALS)
                 .unwrap_or_else(|_| "true".to_string())
                 .parse::<bool>()
                 .unwrap(),
-            api_regex: env::var("FUNES_API_REGEX").unwrap_or_else(|_| r".+".to_string()),
-            h_server: env::var("FUNES_APP").unwrap_or_else(|_| "funes".to_string()),
-            h_user_agent: env::var("FUNES_APP").unwrap_or_else(|_| "funes".to_string()),
+            api_regex: env::var(FUNES_API_REGEX).unwrap_or_else(|_| r".+".to_string()),
+            h_server: env::var(FUNES_APP).unwrap_or_else(|_| "funes".to_string()),
+            h_user_agent: env::var(FUNES_APP).unwrap_or_else(|_| "funes".to_string()),
             latency_collection,
             latency_enable,
-            localhost: env::var("FUNES_HOST").unwrap_or_else(|_| "0.0.0.0:8080".to_string()),
-            log: env::var("FUNES_LOG").unwrap_or_else(|_| "funes,actix_web=info".to_string()),
-            mock_dir: env::var("FUNES_MOCK_DIR").unwrap_or(format!(
+            localhost: env::var(FUNES_HOST).unwrap_or_else(|_| "0.0.0.0:8080".to_string()),
+            log: env::var(FUNES_LOG).unwrap_or_else(|_| "funes,actix_web=info".to_string()),
+            mock_dir: env::var(FUNES_MOCK_DIR).unwrap_or(format!(
                 "{}/.funes",
                 dirs::home_dir().unwrap().to_str().unwrap().to_string()
             )),
